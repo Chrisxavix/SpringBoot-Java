1f97729effad9f6085e9b78b5038044762fd82c5
Chrisxavix <chrischillogallid64@outlook.com>
2021-12-16 23:01:04 -0500

    feat: finish course udemmy

--- src/main/java/mx/com/gm/web/SecurityConfig.java
+++ src/main/java/mx/com/gm/web/SecurityConfig.java
@@ -1,8 +1,11 @@
 package mx.com.gm.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Configuration
 /* @EnableWebSecurity: Habilitar la seguridad web */
@@ -10,5 +13,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 /* Extender de WebSecurityConfigurerAdapter para configurar los usuarios */
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /* Creación de usuarios */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}123")
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password("{noop}123")
+                .roles("USER");
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/editar/**", "/agregar/**", "/eliminar")
+                .hasRole("ADMIN")
+                .antMatchers("/")
+                .hasAnyRole("USER","ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .exceptionHandling().accessDeniedPage("/errores/403");
+    }
 }
